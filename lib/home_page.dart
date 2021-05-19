38e8cd83d2e27e6090bfeeb12645461270b07faf
Ahmad Dzaky Abrori <broridzaky2702@gmail.com>
2021-05-19 18:22:38 +0700

    Adding Height Provider and Weight Provider.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_multi_provider/provider/height_provider.dart';
+import 'package:flutter_multi_provider/provider/weight_provider.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -14,13 +19,19 @@ class HomePage extends StatelessWidget {
                 fontSize: 20,
               ),
             ),
-            Slider(
-              min: 1,
-              max: 100,
-              value: 40,
-              onChanged: (value) {
-                print('Weight: $value');
-              },
+            Consumer<WeightProvider>(
+              builder: (context, weightProvider, _) => Slider(
+                min: 1,
+                max: 100,
+                divisions: 100,
+                label: weightProvider.weight.round().toString(),
+                value: weightProvider.weight,
+                onChanged: (value) {
+                  value = value.roundToDouble();
+                  print('Weight: $value');
+                  weightProvider.weight = value;
+                },
+              ),
             ),
             SizedBox(
               height: 20,
@@ -31,25 +42,35 @@ class HomePage extends StatelessWidget {
                 fontSize: 20,
               ),
             ),
-            Slider(
-              min: 1,
-              max: 200,
-              value: 100,
-              activeColor: Colors.pink,
-              inactiveColor: Colors.pink.withOpacity(0.2),
-              onChanged: (value) {
-                print('Height: $value');
-              },
+            Consumer<HeightProvider>(
+              builder: (context, heightProvider, _) => Slider(
+                min: 1,
+                max: 200,
+                divisions: 100,
+                label: heightProvider.height.round().toString(),
+                value: heightProvider.height,
+                activeColor: Colors.pink,
+                inactiveColor: Colors.pink.withOpacity(0.2),
+                onChanged: (value) {
+                  value = value.roundToDouble();
+                  print('Height: $value');
+                  heightProvider.height = value;
+                },
+              ),
             ),
             SizedBox(
               height: 20,
             ),
-            Text(
-              'Your BMI:\n60.25',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w500,
+            Consumer<WeightProvider>(
+              builder: (context, weightProvider, _) => Consumer<HeightProvider>(
+                builder: (context, heightProvider, _) => Text(
+                  'Your BMI:\n${(weightProvider.weight / pow(heightProvider.height / 100, 2)).toStringAsFixed(2 )}',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
               ),
             ),
           ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_multi_provider/provider/height_provider.dart';
+import 'package:flutter_multi_provider/provider/weight_provider.dart';
+import 'package:provider/provider.dart';
 
 import 'home_page.dart';
 
@@ -10,9 +13,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
+    return ChangeNotifierProvider<WeightProvider>(
+      create: (context) => WeightProvider(),
+      child: ChangeNotifierProvider<HeightProvider>(
+        create: (context) => HeightProvider(),
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: HomePage(),
+        ),
+      ),
     );
   }
 }
