fb07ad7baa80f512a06a383bd879a934b66ea9e9
Ahmad Dzaky Abrori <broridzaky2702@gmail.com>
2021-05-19 18:32:19 +0700

    Consume provider with Provider.of(context).

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,6 +8,9 @@ import 'package:provider/provider.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var weightProvider = Provider.of<WeightProvider>(context);
+    var heightProvider = Provider.of<HeightProvider>(context);
+
     return Scaffold(
       body: Center(
         child: Column(
@@ -19,19 +22,32 @@ class HomePage extends StatelessWidget {
                 fontSize: 20,
               ),
             ),
-            Consumer<WeightProvider>(
-              builder: (context, weightProvider, _) => Slider(
-                min: 1,
-                max: 100,
-                divisions: 100,
-                label: weightProvider.weight.round().toString(),
-                value: weightProvider.weight,
-                onChanged: (value) {
-                  value = value.roundToDouble();
-                  print('Weight: $value');
-                  weightProvider.weight = value;
-                },
-              ),
+            // NOTE: MENGGUNAKAN CONSUMER
+            // Consumer<WeightProvider>(
+            //   builder: (context, weightProvider, _) => Slider(
+            //     min: 1,
+            //     max: 100,
+            //     divisions: 100,
+            //     label: weightProvider.weight.round().toString(),
+            //     value: weightProvider.weight,
+            //     onChanged: (value) {
+            //       value = value.roundToDouble();
+            //       print('Weight: $value');
+            //       weightProvider.weight = value;
+            //     },
+            //   ),
+            // ),
+            Slider(
+              min: 1,
+              max: 100,
+              divisions: 100,
+              label: weightProvider.weight.round().toString(),
+              value: weightProvider.weight,
+              onChanged: (value) {
+                value = value.roundToDouble();
+                print('Weight: $value');
+                weightProvider.weight = value;
+              },
             ),
             SizedBox(
               height: 20,
@@ -42,35 +58,59 @@ class HomePage extends StatelessWidget {
                 fontSize: 20,
               ),
             ),
-            Consumer<HeightProvider>(
-              builder: (context, heightProvider, _) => Slider(
-                min: 1,
-                max: 200,
-                divisions: 100,
-                label: heightProvider.height.round().toString(),
-                value: heightProvider.height,
-                activeColor: Colors.pink,
-                inactiveColor: Colors.pink.withOpacity(0.2),
-                onChanged: (value) {
-                  value = value.roundToDouble();
-                  print('Height: $value');
-                  heightProvider.height = value;
-                },
-              ),
+            // NOTE: MENGGUNAKAN CONSUMER
+            // Consumer<HeightProvider>(
+            //   builder: (context, heightProvider, _) => Slider(
+            //     min: 1,
+            //     max: 200,
+            //     divisions: 100,
+            //     label: heightProvider.height.round().toString(),
+            //     value: heightProvider.height,
+            //     activeColor: Colors.pink,
+            //     inactiveColor: Colors.pink.withOpacity(0.2),
+            //     onChanged: (value) {
+            //       value = value.roundToDouble();
+            //       print('Height: $value');
+            //       heightProvider.height = value;
+            //     },
+            //   ),
+            // ),
+            Slider(
+              min: 1,
+              max: 200,
+              divisions: 100,
+              label: heightProvider.height.round().toString(),
+              value: heightProvider.height,
+              activeColor: Colors.pink,
+              inactiveColor: Colors.pink.withOpacity(0.2),
+              onChanged: (value) {
+                value = value.roundToDouble();
+                print('Height: $value');
+                heightProvider.height = value;
+              },
             ),
             SizedBox(
               height: 20,
             ),
-            Consumer<WeightProvider>(
-              builder: (context, weightProvider, _) => Consumer<HeightProvider>(
-                builder: (context, heightProvider, _) => Text(
-                  'Your BMI:\n${(weightProvider.weight / pow(heightProvider.height / 100, 2)).toStringAsFixed(2 )}',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
+            // NOTE: MENGGUNAKAN CONSUMER
+            // Consumer<WeightProvider>(
+            //   builder: (context, weightProvider, _) => Consumer<HeightProvider>(
+            //     builder: (context, heightProvider, _) => Text(
+            //       'Your BMI:\n${(weightProvider.weight / pow(heightProvider.height / 100, 2)).toStringAsFixed(2)}',
+            //       textAlign: TextAlign.center,
+            //       style: TextStyle(
+            //         fontSize: 24,
+            //         fontWeight: FontWeight.w500,
+            //       ),
+            //     ),
+            //   ),
+            // ),
+            Text(
+              'Your BMI:\n${(weightProvider.weight / pow(heightProvider.height / 100, 2)).toStringAsFixed(2)}',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w500,
               ),
             ),
           ],
